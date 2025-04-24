ec19ffe20816e5fa5d8393c329911f5304910de8
Bjørnar <125570901+Bompilez@users.noreply.github.com>
2025-04-24 14:24:40 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+;(function(){
+  var d=document;
+  var i=d.createElement('iframe');
+  i.src='https://bompilez.github.io/Norges-bondelag/custom.html';
+  i.style.border='none';
+  i.style.width='100%';
+  i.style.height='100%';
+  i.setAttribute('frameborder','0');
+  i.setAttribute('scrolling','no');
+  var s=d.currentScript||d.getElementsByTagName('script')[d.getElementsByTagName('script').length-1];
+  s.parentNode.replaceChild(i, s);
+})();
