083f4492e9eda1b5ab75bb9280fbb1b9cd7d845e
Bjørnar <125570901+Bompilez@users.noreply.github.com>
2025-04-25 09:44:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 ;(function(){
-  var d=document;
-  var i=d.createElement('iframe');
-  i.src='https://bompilez.github.io/Norges-bondelag/custom.html';
-  i.style.border='none';
-  i.style.width='100vw';
-  i.style.height='100vh';
-  i.setAttribute('frameborder','0');
-  i.setAttribute('scrolling','no');
-  var s=d.currentScript||d.getElementsByTagName('script')[d.getElementsByTagName('script').length-1];
+  var d = document;
+  var s = d.currentScript || d.getElementsByTagName('script')[d.getElementsByTagName('script').length - 1];
+  var params = new URLSearchParams(s.src.split('?')[1] || '');
+  var clickURL = params.get('click') || '';
+
+  var i = d.createElement('iframe');
+  i.src = 'https://bompilez.github.io/Norges-bondelag/custom.html?click=' + encodeURIComponent(clickURL);
+  i.style.border = 'none';
+  i.style.width = '100vw';
+  i.style.height = '100vh';
+  i.setAttribute('frameborder', '0');
+  i.setAttribute('scrolling', 'no');
   s.parentNode.replaceChild(i, s);
 })();
