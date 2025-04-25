0737f61627fd9769f57ccd8ea996120a7404c096
Bjørnar <125570901+Bompilez@users.noreply.github.com>
2025-04-25 09:37:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
   var i=d.createElement('iframe');
   i.src='https://bompilez.github.io/Norges-bondelag/custom.html';
   i.style.border='none';
-  i.style.width='100%';
-  i.style.height='100%';
+  i.style.width='100vh';
+  i.style.height='100vh';
   i.setAttribute('frameborder','0');
   i.setAttribute('scrolling','no');
   var s=d.currentScript||d.getElementsByTagName('script')[d.getElementsByTagName('script').length-1];
