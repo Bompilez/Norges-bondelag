0e5011083350ac5f95efe8360291a5caba7c707e
Bjørnar <125570901+Bompilez@users.noreply.github.com>
2025-04-25 10:58:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-;(function(){
+(function(){
   var d = document;
   var s = d.currentScript || d.getElementsByTagName('script')[d.getElementsByTagName('script').length - 1];
-  var params = new URLSearchParams(s.src.split('?')[1] || '');
-  var clickURL = params.get('clickUrlParam') || '';
 
   var i = d.createElement('iframe');
-  i.src = 'https://bompilez.github.io/Norges-bondelag/custom.html?click=' + encodeURIComponent(clickURL);
+  i.src = 'https://bompilez.github.io/Norges-bondelag/custom.html'; // ← legg inn riktig URL her
   i.style.border = 'none';
   i.style.width = '100vw';
   i.style.height = '100vh';
